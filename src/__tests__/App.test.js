61d9fa29231e2e3ed223f991900ab75650198f47
Jeevarajan <senorjeeva@gmail.com>
2024-07-30 11:47:54 +0530

    __tests__ folder created

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 import Renderer from 'react-test-renderer'
 
 test('renders learn react link', () => {
--- src/__tests__/LoginPage.test.js
+++ src/__tests__/LoginPage.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import LoginPage from './LoginPage'
+import LoginPage from '../LoginPage'
 import Renderer from 'react-test-renderer'
 
 
